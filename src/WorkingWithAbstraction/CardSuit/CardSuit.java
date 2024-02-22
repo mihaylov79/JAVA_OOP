597d12b821e0da2b028d5d5fa446e39b2dccacbd
mihaylov79 <misho.mihaylov@gmail.com>
2024-02-23 00:07:48 +0200

    Update

--- src/WorkingWithAbstraction/CardSuit/CardSuit.java
+++ src/WorkingWithAbstraction/CardSuit/CardSuit.java
@@ -2,8 +2,29 @@ package WorkingWithAbstraction.CardSuit;
 
 public enum CardSuit {
 
-    CLUB,
-    DIAMOND,
-    HEARTS,
-    SPADES
+    CLUBS(0),
+    DIAMONDS(1),
+    HEARTS(2),
+    SPADES(3);
+
+    private final int value;
+
+    CardSuit(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static void printSuits(){
+
+        System.out.println("Card Suits:");
+        for (CardSuit cardSuit : CardSuit.values()){
+            System.out.printf("Ordinal value: %d; Name value: %s%n",cardSuit.getValue(),cardSuit);
+        }
+
+
+
+    }
 }
--- src/WorkingWithAbstraction/CardSuit/Main.java
+++ src/WorkingWithAbstraction/CardSuit/Main.java
@@ -3,5 +3,6 @@ package WorkingWithAbstraction.CardSuit;
 public class Main {
     public static void main(String[] args) {
 
+        CardSuit.printSuits();
     }
 }
