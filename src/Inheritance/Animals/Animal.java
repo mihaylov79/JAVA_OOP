eabd780062f9c4a41af21d07162758f3996a73fd
mihaylov79 <misho.mihaylov@gmail.com>
2024-03-05 07:03:59 +0200

    Update

---
+++ src/Inheritance/Animals/Animal.java
@@ -0,0 +1,59 @@
+package Inheritance.Animals;
+
+public class Animal {
+
+    private String name;
+    private int age;
+    private String gender;
+
+    public Animal(String name, int age, String gender) {
+        this.setName(name);
+        this.setAge(age);
+        this.setGender(gender);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()){
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age < 0){
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        if (name == null || gender.trim().isEmpty()){
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.gender = gender;
+    }
+    public String produceSound(){
+        return "";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%n%s %d %s%n%s",this.getClass().getSimpleName(),
+                                this.name,
+                                this.age,
+                                this.gender,
+                                produceSound());
+    }
+}
---
+++ src/Inheritance/Animals/Main.java
@@ -0,0 +1,60 @@
+package Inheritance.Animals;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner kbInput = new Scanner(System.in);
+
+        String commandInput = kbInput.nextLine();
+
+        while(!"Beast!".equalsIgnoreCase(commandInput)){
+
+
+            try{
+            switch (commandInput){
+
+                case "Cat":
+                String[] info = kbInput.nextLine().split("\\s+");
+                Cat cat = new Cat(info[0], Integer.parseInt(info[1]), info[2]);
+                    System.out.println(cat);
+                    break;
+
+                case "Dog":
+                info = kbInput.nextLine().split("\\s+");
+                Dog dog = new Dog(info[0], Integer.parseInt(info[1]), info[2]);
+                    System.out.println(dog);
+                    break;
+
+                case "Frog":
+                    info = kbInput.nextLine().split("\\s+");
+                    Frog frog = new Frog(info[0], Integer.parseInt(info[1]), info[2]);
+                    System.out.println(frog);
+                    break;
+
+                case "Tomcat":
+                    info = kbInput.nextLine().split("\\s+");
+                    Tomcat tomcat = new Tomcat(info[0], Integer.parseInt(info[1]));
+                    System.out.println(tomcat);
+                    break;
+
+                case "Kitten":
+                    info = kbInput.nextLine().split("\\s+");
+                    Kitten kitten = new Kitten(info[0], Integer.parseInt(info[1]));
+                    System.out.println(kitten);
+                    break ;
+            }
+
+            }catch (IllegalArgumentException exception){
+                System.out.println(exception.getMessage());
+            }
+
+
+            commandInput = kbInput.nextLine();
+        }
+
+    }
+}
