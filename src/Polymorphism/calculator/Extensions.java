e1358b970f03b5c29d3bc4570b8037e7b30d1201
mihaylov79 <misho.mihaylov@gmail.com>
2024-03-13 00:09:46 +0200

    Update

--- src/Polymorphism/calculator/Extensions.java
+++ src/Polymorphism/calculator/Extensions.java
@@ -2,10 +2,9 @@ package Polymorphism.calculator;
 
 public class Extensions {
 
-    private Extensions() {
-    }
 
     public static InputInterpreter buildInterpreter(CalculationEngine engine) {
+
         return new InputInterpreter(engine);
     }
 }
