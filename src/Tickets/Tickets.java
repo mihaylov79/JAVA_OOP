ecf2038cd33ed23ac9b43414d1d9d52cfb28ec37
mihaylov79 <misho.mihaylov@gmail.com>
2024-03-13 00:04:21 +0200

    Update

---
+++ src/Tickets/Tickets.java
@@ -0,0 +1,34 @@
+package Tickets;
+
+import java.util.Random;
+
+public enum Tickets {
+    TICKET1 ("Coffee is always a good idea"),
+    TICKET2 ("Never underestimate the importance of being properly CAFFEINATED!"),
+    TICKET3 ("COME TO THE DARKSIDE WE HAVE BLACK COFFEE"),
+    TICKET4 ("Coffee keeps me going until it`s acceptable TO DRINK VODKA!"),
+    TICKET5 ("The problem with COFFEE is trying to MAKE IT when you haven`t had any yet"),
+    TICKET6 ("GIVEN ENOUGH COFFEE, I COULD RULE THE WORLD"),
+    TICKET7 ("Life is SHORT ENJOY your COFFEE!"),
+    TICKET8 ("Everybody should believe in something. I believe I`ll have another COFFEE"),
+    TICKET9 ("Behind every successful person is a substantial amount of Coffee!"),
+    TICKET10 ("RULE #1 - COFFEE ALWAYS COMES FIRST!"),
+    TICKET11 ("COFFEE - PEACE and JUSTICE for all!"),
+    TICKET12 ("COFFEINATING ...... Please wait!");
+
+    private  String ticket;
+
+    Tickets(String ticket) {
+        this.ticket = ticket;
+    }
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    private static Random random = new Random();
+
+    public static Tickets getRandom(){
+        return values()[random.nextInt(values().length)];
+    }
+}
